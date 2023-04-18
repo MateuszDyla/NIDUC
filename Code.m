bed56e4d19cb939447df123fbdf6d829c8c38d40
Mateusz <mdyla6@gmail.com>
2023-04-18 22:53:06 +0200

    change name

--- Code.m
+++
@@ -1,9 +0,0 @@
-classdef Code
-    %CODE Stałe będące identyfikatorami kodów
-    
-    properties
-        TripleCode = 1;
-        Hamming = 2;
-    end
-end
-
---
+++ Constants.m
@@ -0,0 +1,9 @@
+classdef Constants
+    %CODE Stałe będące identyfikatorami kodów
+    
+    properties
+        TripleCode = 1;
+        Hamming = 2;
+    end
+end
+
